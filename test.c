58c984a0b5b4b24d77d010542f05846f8b141fd1
LFayL <88257537+LFayL@users.noreply.github.com>
2021-08-13 18:12:26 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,20 +1,49 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
- 
 #include <stdio.h>
-//int main()
-//{
-//	int a = 10;
-//	scanf("%d", &a);
-//	printf("%d\n", a);
-//	return 0;
-//
-//}
 int main()
 {
-	char arr[5] = { 0 };
-	scanf("%s", arr);
-	printf("%s\n", arr);
-	return 0;
 
 }
+//{
+//	//未声明的标识符
+//	//声明外部符号extern
+//	extern int g_val;
+//	printf("g_val=%d\n", g_val);
+//	return 0;
 
+	/*int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	scanf_s("%d%d", &num1, &num2);
+	sum=num1 + num2;
+	printf("sum=%d\n", sum);
+	return 0;*/
+//}
+//int a = 10;
+//int main()
+//{
+//	/*{
+//		int a = 10;
+//	}*/
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int a = 100;
+//int main()
+//{
+//	/*int a = 10;*/
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int mun2 = 20;
+//int main()
+//{
+//	int num1 = 10;
+//	return 0;
+//}
+//
+//int main()
+//{
+//	short age = 20;
+//float weight = 95.6f;
+//return 0;
+//}
